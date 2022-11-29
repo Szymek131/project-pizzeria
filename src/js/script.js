411a1fc0be9fb58c5a0fea9be7cdf0979a4069bf
Szymon Wiktor <sz.wiktor131@gmail.com>
2022-11-29 22:32:08 +0100

    add processOrder method in JS

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,9 @@
       thisProduct.renderInMentu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
+
 
       console.log('new Product: ', thisProduct);
     }
@@ -101,8 +104,6 @@
       const thisProduct = this;
   
       /* find the clickable trigger (the element that should react to clicking) */
-      
-      //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
   
       /* START: add event listener to clickable trigger on event click */
       
@@ -115,7 +116,6 @@
         /* find active product (product that has active class) */
         
         const activeProduct = document.querySelector(select.all.menuProductsActive);
-        console.log(activeProduct);
         
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         
@@ -126,11 +126,70 @@
         /* toggle active class on thisProduct.element */
 
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
-        console.log(thisProduct);
 
       });
   
     }
+
+    initOrderForm(){
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      console.log('initOrderForm: ', thisProduct);
+    }
+
+    processOrder() {
+      const thisProduct = this;
+    
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+    
+      // set price to default price
+      let price = thisProduct.data.price;
+    
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+    
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+
+          if(formData[paramId] && formData[paramId].includes(optionId)){
+
+            if(option['default'] == null){
+              price += option['price'];
+            }
+          } else {
+            if(option['default'] == true){
+              price -= option['price'];
+            }
+          }
+        }
+      }
+    
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+    }
+
   }
     
   const app = {
