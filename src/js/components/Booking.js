9bedb34b152e047c1a778e05467963be10c9ade5
Szymon Wiktor <sz.wiktor131@gmail.com>
2022-12-26 23:58:09 +0100

    add hour and date picker in JS

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -2,6 +2,7 @@ import {templates, select} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import utils from '../utils.js';
 import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking{
   constructor(element){
@@ -23,6 +24,8 @@ class Booking{
       wrapper: element,
       peopleAmount: element.querySelector(select.booking.peopleAmount),
       hoursAmount: element.querySelector(select.booking.hoursAmount),
+      datePicker: element.querySelector(select.widgets.datePicker.wrapper),
+      hourPicker: element.querySelector(select.widgets.hourPicker.wrapper)
     };
   }
 
@@ -31,6 +34,8 @@ class Booking{
 
     thisBooking.peopleAmountWidget =  new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
     
     thisBooking.dom.wrapper.addEventListener('updated', function(event){
       event.preventDefault();
